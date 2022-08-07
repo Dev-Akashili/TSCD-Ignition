d104d762eb67a77ed05394cb9bfe400e3cb952fc
Akashili Emeka <94780632+Dev-Akashili@users.noreply.github.com>
2022-08-08 00:03:42 +0100

    Cors Permission Bug Fix

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { Logger } from '@nestjs/common';
 async function bootstrap() {
   const logger = new Logger();
   const app = await NestFactory.create(AppModule);
-  app.enableCors({ credentials: true, origin: 'http://localhost:3000' });
+  app.enableCors();
   const port = process.env.PORT || 5000;
   await app.listen(port);
   logger.log(`Application is running and listening on port ${port}`);
